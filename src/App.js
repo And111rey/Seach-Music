5d54a54a2f21bffe8b6cd085086f82ef2197ad34
And111rey <samu111ray@gmail.com>
2019-05-18 22:20:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "./app.css";
+import { FormGroup, FormControl, InputGroup, Button,Glyphicon } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: ""
+    }
+  }
+
+  search() {
+    console.log("This state - ", this.state);
+  }
+
+  render () {
+    return(
+      <div className="app">
+        <div className="app-title">Music Master</div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl 
+              type="text" 
+              placeholder="Search Artist"
+              query={this.state.query}
+              onChange={ event => {this.setState({query: event.target.value})} }
+            />
+             <InputGroup.Append>
+                <Button onClick={() => this.search()} variant="outline-secondary">Search</Button>
+              </InputGroup.Append>     
+          </InputGroup>
+          
+
+        </FormGroup>
+        <div className="propfile">
+          <div>Artist Picture</div>
+          <div>Artist name</div>
+        </div>
+        <div className="gallery">
+          Gallery
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+// lesson 23
+
+export { App }
